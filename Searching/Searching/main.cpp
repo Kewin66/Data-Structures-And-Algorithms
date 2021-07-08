3c63da88b7415bc6ec61c3bb1c32bce93ca06a9b
Kewin V <kewindev96@gmail.com>
2021-07-08 17:53:07 +0530

    Searching: Fixed Corner cases for Binary Search
    Implemented iterative and recursive implementation of Binary Search. Also, experimented STL implementation of lower_bound, upper_bound, binary_search methods. lower_bound returns first occurrence, upper_bound returns next greater element of the element to be searched. NOTE: Always subtract the result of lower_bound/upper_bound with vec.begin(). Upper_bound-lower_bound gives the number of occurences and if lower_bound==upper_bound then element is not present in the input.

--- Searching/Searching/main.cpp
+++ Searching/Searching/main.cpp
@@ -7,28 +7,40 @@
 
 #include <iostream>
 #include <vector>
-
+#include <algorithm>
+#include <chrono>
+#include <ctime>
+#define DISPLAY
 #define SIZE 1000
+#define BOUND_DISPLAY
 using namespace std;
-void bs(vector<int> inp, int low, int high,int find_element){
-    int mid = (low+high)/2;
+int BinarySearch(vector<int> inp, int low, int high,int find_element){
+    
+    if (high>=low) {
+    int mid = low+((high-low)/2); //(low+high)/2;
+#ifndef BOUND_DISPLAY
+    cout<<"Low:"<<low<<"  "<<"High:"<<high<<endl;
+    cout<<"Mid element:"<<mid<<" Actual Mid:"<<low+((high-low)/2)<<endl;
+        cout<<"First element"<<inp[0]<<endl;
+#endif
     if(inp[mid]==find_element) {
         cout<<"Element found at pos:"<<mid<<endl;
-        return;
+        return mid;
     }
     else if(inp[mid]<find_element) {
-        bs(inp,mid+1,high,find_element);
-        return;
+        return BinarySearch(inp,mid+1,high,find_element);
         
     } else {
-        bs(inp,low,mid-1,find_element);
-        return;
+        return BinarySearch(inp,low,mid-1,find_element);
     }
-    
+    }
+    return -1;
 }
 int main(int argc, const char * argv[]) {
     // insert code here...
-    vector<int> vec;
+    chrono::time_point<chrono::system_clock>start,end;
+    vector<int> vec = {55,72,6,89,42,12,12,12,12,12,12,12,7,7};
+#ifndef DISPLAY
     for (int i=0;i<SIZE/2;i++) {
         int a = rand();
         vec.push_back(a);
@@ -38,7 +50,59 @@ int main(int argc, const char * argv[]) {
         int a = rand();
         vec.push_back(a);
     }
+#endif
     sort(vec.begin(),vec.end());
-    bs(vec,0,SIZE-1,66);
+    for(int e:vec){
+        cout<<e<<" ";
+    }
+    cout<<endl;
+    int high = (int)vec.size()-1;
+    start = chrono::system_clock::now();
+    int found_index = BinarySearch(vec,0,high,8);
+    end = chrono::system_clock::now();
+    if(found_index==-1) {
+        cout<<"Element is not present in the array.\n";
+    }
+    chrono::duration<double> elapsed_time = end-start;
+    time_t end_time = chrono::system_clock::to_time_t(end);
+    cout<<"Elapsed time for BinarySearch recursive implementation: "<<elapsed_time.count()<<endl;
+    cout<<"Computation completed at "<<ctime(&end_time)<<endl;
+//Iterative method to do Binary search
+    int low = 0;
+    int find_element = 8;
+    cout<<"************Iterative Binary search*********"<<endl;
+    bool flag = false;
+    while(low<=high) {
+        int mid = low+ (high-low)/2;
+        if(vec[mid]==find_element) {
+            cout<<"Element found at:"<<mid<<endl;
+            flag=true;
+            break;
+        }
+        else if (vec[mid]<find_element){
+            low = mid+1;
+            continue;
+        }
+        else {
+            high=mid-1;
+            continue;
+        }
+    }
+    if(!flag) {
+        cout<<"Element isn't present in the given input.\n\n\n";
+    }
+    cout<<"///////////STL IMPLEMENTATION//////////////\n";
+//STL implementation of binary search
+    //Binary Search returns if element is present or not in the input array.
+    cout<<"Element is present "<<binary_search(vec.begin(),vec.end(),find_element)<<" using STL Binary search method."<<endl;
+    
+//STL implementatio binary search to return Index
+    auto element_first_occurence = lower_bound(vec.begin(),vec.end(),find_element)-vec.begin();
+    auto element_last_occurence = upper_bound(vec.begin(),vec.end(),find_element)-vec.begin();
+    cout<<"Element found using lower_bound at "<<element_first_occurence<<endl;
+    cout<<"Element found using upper_bound at"<<element_last_occurence<<endl;
+    
+    cout<<"Number of occurrences:"<<element_last_occurence-element_first_occurence<<endl;
+
     return 0;
 }
