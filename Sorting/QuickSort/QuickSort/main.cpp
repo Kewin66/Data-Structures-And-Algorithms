49d50993418d8e93f2d0b1206e3d5f3774b2db5b
Kewin V <kewindev96@gmail.com>
2021-07-18 16:27:19 +0530

    Sort: Streak

---
+++ Sorting/QuickSort/QuickSort/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  QuickSort
+//
+//  Created by Kewin Srinath on 7/18/21.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    vector <int> vec;
+    for(int i =0;i<1000;i++) {
+        vec[i]=rand();
+    }
+    return 0;
+}
