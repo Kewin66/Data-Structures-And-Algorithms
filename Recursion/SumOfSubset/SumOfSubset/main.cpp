4465d69ed781d5dc4f6ff9fa8971ff1373e7f36f
Kewin V <kewindev96@gmail.com>
2021-07-23 15:30:32 +0530

    Recursion: Subset

---
+++ Recursion/SumOfSubset/SumOfSubset/main.cpp
@@ -0,0 +1,40 @@
+//
+//  main.cpp
+//  SumOfSubset
+//
+//  Created by Kewin Srinath on 7/23/21.
+//
+
+#include <iostream>
+#include<vector>
+
+using namespace std;
+
+int findsubsets(const vector<int> &a,int sum, int index, int subsum)
+{
+    static int count = 0;
+    if(index == a.size())
+    {   int temp = 0;
+        /*for(auto e:b) {
+            temp+=e;
+            cout<<e<<" ";
+        }
+        cout<<endl;*/
+        if(temp == subsum ) count++;
+        return 1;
+    }
+    //b.push_back(a[index]);
+    findsubsets(a, sum, index+1,subsum+a[index]);
+    //b.pop_back();
+    findsubsets(a,sum, index+1,subsum-a[index]);
+    
+    return count;
+}
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    vector<int>a = {10,20,15};
+    vector<int> t = {};
+    int sum = 37;
+    cout<<"Number of subsets with sum 4: "<<findsubsets(a,sum,0,0)<<endl;
+    return 0;
+}
