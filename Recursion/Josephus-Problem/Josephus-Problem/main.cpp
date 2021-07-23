99447adc4cb5a0f606a0b0d23b01951bae2d02f3
Kewin V <kewindev96@gmail.com>
2021-07-23 14:54:16 +0530

    Recursion: JP problem

--- Recursion/Josephus-Problem/Josephus-Problem/main.cpp
+++ Recursion/Josephus-Problem/Josephus-Problem/main.cpp
@@ -6,9 +6,39 @@
 //
 
 #include <iostream>
+#include <vector>
 
+using namespace std;
+/*vector<int> jp(vector<int> n, int k, int start, int alive)
+{
+    if(alive == 1) return n;
+    int dead = (start + (k-1))%n.size();
+    
+    while(n[dead] != -1) {dead++;}
+    dead %= n.size();
+    cout<<"Dead:"<<dead<<" ";
+    return jp(n,k,(dead+1)%n.size(),alive-1);
+    
+}*/
+
+int jp(int n , int k)
+{
+    if(n ==1) return 0;
+    
+    return ((jp(n-1,k)+k)%n);
+    
+}
+
+int myjp(int n , k)
+{
+    
+    return jp(n,k)+1; // if Circle is numbered from 1.
+}
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+   // vector<int> n(7,0);
+    //vector<int> res = jp(n,3,0,7);
+    
+    cout<<"\nAlive person:"<<jp(7,3)<<endl;
     return 0;
 }
