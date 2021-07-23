d0692b99d9cbcd9138663e08a1549ca18e32b960
Kewin V <kewindev96@gmail.com>
2021-07-23 16:17:05 +0530

    Recursion: Subset

--- Recursion/SumOfSubset/SumOfSubset/main.cpp
+++ Recursion/SumOfSubset/SumOfSubset/main.cpp
@@ -14,19 +14,26 @@ int findsubsets(const vector<int> &a,int sum, int index, int subsum)
 {
     static int count = 0;
     if(index == a.size())
-    {   int temp = 0;
+    {   //int temp = 0;
         /*for(auto e:b) {
             temp+=e;
             cout<<e<<" ";
         }
         cout<<endl;*/
-        if(temp == subsum ) count++;
+        if(sum == subsum ) count++;
         return 1;
     }
     //b.push_back(a[index]);
+    cout<<"\nsubsum+a[index]"<<subsum+a[index]<<" ";
     findsubsets(a, sum, index+1,subsum+a[index]);
     //b.pop_back();
+    cout<<"\nIndex: "<<index;
+    cout<<"\nsubsum-a[index]"<<subsum-a[index]<<" ";
     findsubsets(a,sum, index+1,subsum-a[index]);
+    
+    
+    //if(n-1) == 0 return sum == 0 ?1:0;
+    // return subsets(a,n-1,subsum)+subsets(a,n-1,subsum+a[n-1])
     
     return count;
 }
@@ -34,7 +41,7 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     vector<int>a = {10,20,15};
     vector<int> t = {};
-    int sum = 37;
+    int sum = 25;
     cout<<"Number of subsets with sum 4: "<<findsubsets(a,sum,0,0)<<endl;
     return 0;
 }
