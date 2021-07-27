84095ed4c81b5f8b35cbf005563d58b2614806ed
Kewin V <kewindev96@gmail.com>
2021-07-27 12:48:08 +0530

    Recursion:GeeksForGeeks

--- Recursion/GeeksForGeeks-RecursionProblems/GeeksForGeeks-RecursionProblems/main.cpp
+++ Recursion/GeeksForGeeks-RecursionProblems/GeeksForGeeks-RecursionProblems/main.cpp
@@ -17,8 +17,18 @@ void printnos(int N)
     
     
 }
+
+int sumOfDigits(int n)
+{
+    //Your code here
+    if(n<=1) return n;
+    
+    return (n%10)+sumOfDigits(n/10);
+    
+}
 int main(int argc, const char * argv[]) {
     // insert code here...
+    cout<<"Sum of digits: "<<sumOfDigits(123455)<<endl;
     printnos(10);
     return 0;
 }
